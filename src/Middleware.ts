604858b411729d3a12d4101f4a8a73a2ac8f4b22
AA_Alex <nasonoaa@63pokupki.ru>
2025-01-09 07:54:13 +0400

    Протетситрован модуль событий, доабвлены логи на запрос ответ в мидлвар

--- src/Middleware.ts
+++ src/Middleware.ts
@@ -65,50 +65,52 @@ export function faAuthSysMiddleware(level: number): any {
             if (respStatus === 200) {
                 req.body.curr_user = parsedUserData?.id || 0;
 
-                // Спизжено отсюда https://yflooi.medium.com/nestjs-request-and-response-logging-with-middleware-b486121e4907
-                // не понятно, но работает - рисует ответ любого метода в логи
-                const getResponseLog = (res: Response) => {
-                    const rawResponse = res.write;
-                    const rawResponseEnd = res.end;
-                    const chunkBuffers = [];
-                    res.write = (...chunks) => {
-                        const resArgs = [];
-                        for (let i = 0; i < chunks.length; i++) {
-                            resArgs[i] = chunks[i];
-                            if (!resArgs[i]) {
-                                res.once('drain', res.write);
-                                i--;
-                            }
-                        }
-                        if (resArgs[0]) {
-                            chunkBuffers.push(Buffer.from(resArgs[0]));
-                        }
-                        return rawResponse.apply(res, resArgs);
-                    };
-                    res.end = (...chunk) => {
-                        const resArgs = [];
-                        for (let i = 0; i < chunk.length; i++) {
-                            resArgs[i] = chunk[i];
-                        }
-                        if (resArgs[0]) {
-                            chunkBuffers.push(Buffer.from(resArgs[0]));
-                        }
-                        const body = Buffer.concat(chunkBuffers).toString('utf8');
-                        const responseLog = body || {};
 
-                        console.log('Ответ: >> ', responseLog);
-                        rawResponseEnd.apply(res, resArgs); // ,без этого запрос не заканчивается
-
-                        return responseLog as unknown as Response;
-                    };
-                };
-
-                getResponseLog(resp)
-                console.timeEnd('Время запроса: >>')
                 next();
             } else {
                 throw new HttpException('Ошибка доступа', HttpStatus.INTERNAL_SERVER_ERROR);
             }
+
+            // Спизжено отсюда https://yflooi.medium.com/nestjs-request-and-response-logging-with-middleware-b486121e4907
+            // не понятно, но работает - рисует ответ любого метода в логи
+            const getResponseLog = (res: Response) => {
+                const rawResponse = res.write;
+                const rawResponseEnd = res.end;
+                const chunkBuffers = [];
+                res.write = (...chunks) => {
+                    const resArgs = [];
+                    for (let i = 0; i < chunks.length; i++) {
+                        resArgs[i] = chunks[i];
+                        if (!resArgs[i]) {
+                            res.once('drain', res.write);
+                            i--;
+                        }
+                    }
+                    if (resArgs[0]) {
+                        chunkBuffers.push(Buffer.from(resArgs[0]));
+                    }
+                    return rawResponse.apply(res, resArgs);
+                };
+                res.end = (...chunk) => {
+                    const resArgs = [];
+                    for (let i = 0; i < chunk.length; i++) {
+                        resArgs[i] = chunk[i];
+                    }
+                    if (resArgs[0]) {
+                        chunkBuffers.push(Buffer.from(resArgs[0]));
+                    }
+                    const body = Buffer.concat(chunkBuffers).toString('utf8');
+                    const responseLog = body || {};
+
+                    console.log('Ответ: >> ', responseLog);
+                    rawResponseEnd.apply(res, resArgs); // ,без этого запрос не заканчивается
+
+                    return responseLog as unknown as Response;
+                };
+            };
+
+            getResponseLog(resp)
+            console.timeEnd('Время запроса: >>')
 
             console.log('\n\n');
         }
